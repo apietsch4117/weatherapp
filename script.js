720b9b50cf472268795f8e6faedfeacb43d5eafa
Amanda Pietsch <amanda.ann.pietsch@gmail.com>
2022-11-24 19:27:15 -0800

    small edits

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=94563c94df882a6083a9ac3f597f1472")
+fetch("https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=35&appid=94563c94df882a6083a9ac3f597f1472")
 .then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
